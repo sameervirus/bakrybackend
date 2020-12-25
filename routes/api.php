0abedd7b810717484218a65b87d3513892fda6a0
Samir Nabil <samir.arcons@yahoo.co.uk>
2020-12-25 21:09:45 +0200

    Add Passport

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login', [PassportController::class, 'login']);
+Route::post('register', [PassportController::class, 'register']);
+ 
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', [PassportController::class, 'details']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
